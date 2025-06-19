665e52b343420eb1d3a06d0ddd44f6e377be4d57
we <mviriasov@>
2025-06-19 11:38:00 +0300

    new Test

---
+++ src/test/java/DgDtest.java
@@ -0,0 +1,43 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class DgDtest {
+
+    @Test
+    void dgdTest() {
+
+        //Открыть https://the-internet.herokuapp.com/drag_and_drop
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        //Перенести тругольник А на место В
+
+        //actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().build().perform();
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+
+        //Проверьте, что прямоугольники действительно поменялись
+
+        $("#column-a").shouldHave(Condition.text("B"));
+
+
+        sleep(5000);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
