0414d052e0252b529ff0e2e30678f6897a8b38f2
we <mviriasov@>
2025-06-21 09:55:18 +0300

    add radio and number

--- src/test/java/Demoqaform.java
+++ src/test/java/Demoqaform.java
@@ -1,9 +1,9 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.lang.module.Configuration;
-
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.sleep;
 
@@ -12,13 +12,14 @@ public class Demoqaform {
     @BeforeAll
     static void beforeAll(){
 
-
+        Configuration.pageLoadTimeout = 60000;
 
     }
 
 
     @Test
     void DemoForm() {
+
 
         //Открытие формы
         Selenide.open("https://demoqa.com/automation-practice-form");
@@ -27,9 +28,10 @@ public class Demoqaform {
         $("#firstName").setValue("Max");
         $("#lastName").setValue("Bochkin");
         $("#userEmail").setValue("parararapa@mail.ru");
-        $("");
-
-
+        //Выбор гендера
+        // с помощью Радиобаттон$(".custom-control-input").selectRadio("Male");
+        $(byText("Male")).click();
+        $("#userNumber").setValue("7927388223");
 
 
 
