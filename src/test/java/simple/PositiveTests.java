e313ec7622525290573ede963bbac19288a730c2
we <mviriasov@>
2025-09-08 12:27:43 +0300

    Jenkins test

---
+++ src/test/java/simple/PositiveTests.java
@@ -0,0 +1,30 @@
+package simple;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@Tag("simple")
+public class PositiveTests {
+
+    @Test
+    void test1() {
+        assertTrue(true);
+    }
+    @Test
+    void test2() {
+        assertTrue(true);
+    }
+    @Test
+    void test3() {
+        assertTrue(true);
+    }
+    @Test
+    void test4() {
+        assertTrue(true);
+    }
+    @Test
+    void test5() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/simple/SkippedTests.java
@@ -0,0 +1,24 @@
+package simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@Tag("simple")
+public class SkippedTests {
+
+    @Test
+    @Disabled
+    void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled ("some reason")
+    void test2() {
+        assertTrue(true);
+    }
+
+}
+
